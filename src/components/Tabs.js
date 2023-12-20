21f71f9445de71f940d93108edbcf8da19af0eba
Fernando Cordeiro <fernando_cordeiro@live.com>
2023-12-20 15:31:25 +0000

    📦 NEW: Add bottom navigation tabs for Current, Upcoming, and City screens

---
+++ src/components/Tabs.js
@@ -0,0 +1,48 @@
+/* eslint-disable react/no-unstable-nested-components */
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather } from '@expo/vector-icons';
+import CurrentWeather from '../screens/CurrentWeather';
+import UpcomingWeather from '../screens/UpcomingWeather';
+import City from '../screens/City';
+
+const TabBarIcon = ({ focused, name }) => (
+  <Feather name={name} size={25} color={focused ? 'tomato' : 'black'} />
+);
+
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen
+        name="Current"
+        component={CurrentWeather}
+        options={{
+          tabBarIcon: (props) => <TabBarIcon {...props} name="droplet" />,
+        }}
+      />
+      <Tab.Screen
+        name="Upcoming"
+        component={UpcomingWeather}
+        options={{
+          tabBarIcon: (props) => <TabBarIcon {...props} name="clock" />,
+        }}
+      />
+      <Tab.Screen
+        name="City"
+        component={City}
+        options={{
+          tabBarIcon: (props) => <TabBarIcon {...props} name="home" />,
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default Tabs;
